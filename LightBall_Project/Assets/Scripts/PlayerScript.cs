1661598cb9e395fceef46e27b528d263434191ff
Coehl Gleckner (RIT Student) <crg5646@ad.rit.edu>
2018-10-04 08:24:49 -0400

    Commiting creating files
    Folders and files

---
+++ LightBall_Project/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    // attributes
+    OrbScript orbScript;
+    InputManagerScript inputManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}

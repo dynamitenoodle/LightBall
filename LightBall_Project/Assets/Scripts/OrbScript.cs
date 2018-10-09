dc7ec87c2793f9d50b0531bd0f6edb12d2502941
dynamitenoodle <dynamitenoodle@gmail.com>
2018-10-09 16:48:08 -0400

    Player and orb have basic implementation
    Needs normal maps for lighting, and other features

--- LightBall_Project/Assets/Scripts/OrbScript.cs
+++ LightBall_Project/Assets/Scripts/OrbScript.cs
@@ -16,30 +16,40 @@ public class OrbScript : Entity {
 	void Start ()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-		heldPosition = transform.position = player.transform.GetChild(0).gameObject.transform.position;
+		heldPosition = player.transform.GetChild(0).gameObject.transform.position;
+		transform.position = heldPosition = new Vector3(heldPosition.x, heldPosition.y, transform.position.z); // sets the position of the orb and the held position with correct z positions
 
+		// setting colliders
 		orbCol = GetComponent<Collider2D>();
 		playerCol = player.GetComponent<Collider2D>();
 		orbCatchCol = player.transform.GetChild(0).gameObject.GetComponent<Collider2D>();
 
+		// setting other variables to have default states
 		pos = transform.position;
 		vel = Vector3.zero;
 		acc = Vector3.zero;
 		isHeld = true;
 		canPickup = true;
 		pickupTimer = 0;
-		pickupTimerMax = 120;
-
+		pickupTimerMax = 60;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
+		// Check if the player is holding the orb
 		if (CheckHold())
 			pos = heldPosition;
+		// it isn't so slow down
+		else
+			vel *= .9f;
 
+		// If the orb can't be picked up yet
 		if (!canPickup)
 		{
+			if (GetComponent<SpriteRenderer>().color != Color.red)
+				GetComponent<SpriteRenderer>().color = Color.red;
+
 			if (pickupTimer > pickupTimerMax)
 			{
 				canPickup = true;
@@ -47,6 +57,9 @@ public class OrbScript : Entity {
 			}
 			pickupTimer++;
 		}
+		else
+			if (GetComponent<SpriteRenderer>().color != Color.yellow)
+				GetComponent<SpriteRenderer>().color = Color.yellow;
 
 		UpdatePosition();
 	}
@@ -56,6 +69,7 @@ public class OrbScript : Entity {
 	{
 		// update where the ball should be
 		heldPosition = player.transform.GetChild(0).gameObject.transform.position;
+		heldPosition = new Vector3(heldPosition.x, heldPosition.y, transform.position.z);
 
 		// check if we should be or are being held
 		if (isHeld)
@@ -67,7 +81,6 @@ public class OrbScript : Entity {
 			{
 				if (playerCol.Distance(orbCol).distance < 0 || orbCatchCol.Distance(orbCol).distance < 0)
 				{
-					Debug.Log("PICK IT UP KID");
 					isHeld = true;
 					return true;
 				}
@@ -83,6 +96,11 @@ public class OrbScript : Entity {
 		isHeld = false;
 		canPickup = false;
 
-		Debug.Log(player.transform.forward);
+		Vector3 direction;
+		direction = player.transform.GetChild(0).gameObject.transform.position - player.transform.position;
+		direction.z = 0;
+
+		direction *= (force);
+		acc = direction;
 	}
 }

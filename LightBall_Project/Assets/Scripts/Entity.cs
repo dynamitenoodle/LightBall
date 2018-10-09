4dd827e603172d7857977aced178447330a43c6d
dynamitenoodle <dynamitenoodle@gmail.com>
2018-10-09 12:47:29 -0400

    Created basic orb and player
    no throwing yet tho

---
+++ LightBall_Project/Assets/Scripts/Entity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Entity : MonoBehaviour {
+
+	// attrbutes
+	[HideInInspector]
+	public Vector3 pos, vel, acc;
+	public float maxVel, force;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// updating the players position
+	public void UpdatePosition()
+	{
+		vel += acc;
+		vel = Vector3.ClampMagnitude(vel, maxVel);
+		pos += vel;
+		transform.position = pos;
+		acc = Vector3.zero;
+	}
+}
--- LightBall_Project/Assets/Scripts/OrbScript.cs
+++ LightBall_Project/Assets/Scripts/OrbScript.cs
@@ -2,20 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OrbScript : MonoBehaviour {
+public class OrbScript : Entity {
 
     // attributes
     GameObject player;
+	Vector3 heldPosition;
+	public bool isHeld;
+	bool canPickup;
+	int pickupTimer, pickupTimerMax;
+	Collider2D orbCol, orbCatchCol, playerCol;
 
 	// Use this for initialization
 	void Start ()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+		heldPosition = transform.position = player.transform.GetChild(0).gameObject.transform.position;
+
+		orbCol = GetComponent<Collider2D>();
+		playerCol = player.GetComponent<Collider2D>();
+		orbCatchCol = player.transform.GetChild(0).gameObject.GetComponent<Collider2D>();
+
+		pos = transform.position;
+		vel = Vector3.zero;
+		acc = Vector3.zero;
+		isHeld = true;
+		canPickup = true;
+		pickupTimer = 0;
+		pickupTimerMax = 120;
+
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
     {
-		
+		if (CheckHold())
+			pos = heldPosition;
+
+		if (!canPickup)
+		{
+			if (pickupTimer > pickupTimerMax)
+			{
+				canPickup = true;
+				pickupTimer = 0;
+			}
+			pickupTimer++;
+		}
+
+		UpdatePosition();
+	}
+
+	// Checks if we are holding the ball and if we should pick it up
+	bool CheckHold()
+	{
+		// update where the ball should be
+		heldPosition = player.transform.GetChild(0).gameObject.transform.position;
+
+		// check if we should be or are being held
+		if (isHeld)
+			return true;
+
+		else
+		{
+			if (canPickup)
+			{
+				if (playerCol.Distance(orbCol).distance < 0 || orbCatchCol.Distance(orbCol).distance < 0)
+				{
+					Debug.Log("PICK IT UP KID");
+					isHeld = true;
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	// tosses the orb
+	public void ThrowOrb()
+	{
+		isHeld = false;
+		canPickup = false;
+
+		Debug.Log(player.transform.forward);
 	}
 }

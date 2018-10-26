154b82daf239ed1766e62fb6ab65f927c64e79de
Chris Commisso <qwertykeebored@live.com>
2018-10-26 11:24:08 -0400

    more roomscript stuff

--- LightBall_Project/Assets/Scripts/RoomScript.cs
+++ LightBall_Project/Assets/Scripts/RoomScript.cs
@@ -3,6 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomScript : MonoBehaviour {
+    public enum RoomType
+    {
+        deadEndUp,
+        deadEndDown,
+        deadEndRight,
+        deadEndLeft,
+        hallwayUpDown,
+        hallwayLeftRight,
+        TriUp,
+        TriDown,
+        TriLeft,
+        TriRight,
+        Quad,
+    }
     public RoomScript up;
     public RoomScript down;
     public RoomScript left;
